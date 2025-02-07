f87f0e9a9f91bf6664759ccc4aa4a849fb5f678a
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-02-07 17:18:23 +0800

    update api file

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\API\FileController;
 use App\Http\Controllers\API\RegisterController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(RegisterController::class)->group(function () {
@@ -11,8 +9,3 @@ Route::controller(RegisterController::class)->group(function () {
     Route::post('login', 'login');
 });
 Route::apiResource('/files', FileController::class);
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/login', LoginController::class);
-    Route::post('/logout', LogoutController::class);
-}
-);
